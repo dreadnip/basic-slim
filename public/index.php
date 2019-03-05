934497b9972b51e162ec2e5ce2cc57595c80dd5e
dreadnip <sanderdlm69@gmail.com>
2019-03-05 10:57:50 +0100

    Added controler by default

--- public/index.php
+++ public/index.php
@@ -23,6 +23,9 @@ require __DIR__ . '/../src/dependencies.php';
 // Register middleware
 require __DIR__ . '/../src/middleware.php';
 
+// Register custom controller
+require __DIR__ . '/../src/controller.php';
+
 // Register routes
 require __DIR__ . '/../src/routes.php';
 
